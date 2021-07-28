d8828165c5000f7f2b58cd35458977ab58dfb3fb
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-28 11:49:43 +0800

    Arrow Function

--- 1 ES.js
+++ 1 ES.js
@@ -81,3 +81,13 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
   }
   const PI = freezeObj();
+
+//   --
+//   Use Arrow Functions to Write Concise Anonymous Functions
+//   >
+//   Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
+//// var magic = function() {
+//   return new Date();
+// };
+
+const magic = () => new Date();
