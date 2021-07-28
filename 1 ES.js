7adf8566eca400b95e630360d618d19cb2031f30
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-28 12:02:07 +0800

    Arrow Function with parameters

--- 1 ES.js
+++ 1 ES.js
@@ -91,3 +91,15 @@ function freezeObj() {
 // };
 
 const magic = () => new Date();
+
+// --
+// Write Arrow Functions with Parameters
+// >
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+////var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
