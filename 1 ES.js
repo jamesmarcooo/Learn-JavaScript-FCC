b15dea2978630c9260a6b4e7cff53fd458f681c7
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-28 11:27:53 +0800

    var and let

--- 1 ES.js
+++ 1 ES.js
@@ -11,3 +11,19 @@ function catTalk() {
 }
 catTalk();
 
+// --
+// Compare Scopes of the var and let Keywords
+// When you declare a variable with the let keyword inside a block, statement, or expression, its scope is limited to that block, statement, or expression.
+// >
+// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+  
