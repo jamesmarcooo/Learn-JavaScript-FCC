1d2ca4bdd62df5ff468682aad9e0a245264c25f0
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 15:40:13 +0800

    JavaScript Promise

--- 1 ES.js
+++ 1 ES.js
@@ -384,3 +384,9 @@ import subtract from "./math_functions.js"
 subtract(7,4);
 
 
+// --
+// Create a JavaScript Promise
+// >
+// Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+const makeServerRequest = new Promise((resolve,reject) => {});
+
