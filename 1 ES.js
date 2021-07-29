7571f736d7e406459e0e85404c29da71bc7215ec
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 16:08:31 +0800

    Handle a Rejected Promise with catch

--- 1 ES.js
+++ 1 ES.js
@@ -425,3 +425,23 @@ const makeServerRequest = new Promise((resolve, reject) => {
 });
 
 
+// --
+// Handle a Rejected Promise with catch
+// >
+// Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received").catch(error =>{
+      console.log(error);
+    });
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
