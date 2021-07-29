2cf639129129063b465efa6a42bdee8c5f4212e4
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 07:06:15 +0800

    Match anything w/ wildcard period .

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -43,7 +43,13 @@ let starRegex = /Twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
 
 
-
+// --
+// Match Anything with Wildcard Period
+// >
+// Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
 
 
 
