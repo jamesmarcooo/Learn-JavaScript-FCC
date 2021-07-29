55d0b3ecfd1a300a4618da55d156f05c57c25e3a
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 14:22:11 +0800

    Pass an Object as a Function's Parameters

--- 1 ES.js
+++ 1 ES.js
@@ -197,3 +197,26 @@ function removeFirstTwo(list) {
 }
 const arr = removeFirstTwo(source);
 
+//--
+//Use Destructuring Assignment to Pass an Object as a Function's Parameters
+//>
+//Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+//const half = (stats) => (stats.max + stats.min) / 2.0; 
+const half = ({max, min}) => {
+    return (max + min)/2.0;
+}
+
+
+//--
+//
+//>
+//
