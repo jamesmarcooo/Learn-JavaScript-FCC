7e9f6991d52de64ab1782df478d40534a9ce186e
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 15:36:24 +0800

    Export Default for fallback value

--- 1 ES.js
+++ 1 ES.js
@@ -364,3 +364,14 @@ import * as stringFunctions from "./string_functions.js";
 
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
+
+
+// --
+// Create an Export Fallback with export default
+// >
+// The following function should be the fallback value for the module. Please add the necessary code to do so.
+/*function subtract(x, y)*/
+export default function(x, y) {
+  return x - y;
+}
+
