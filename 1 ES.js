8590f0593de845a8c38697021e55992dfee8003f
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 16:06:49 +0800

    Handle a Fulfilled Promise with then

--- 1 ES.js
+++ 1 ES.js
@@ -406,3 +406,22 @@ const makeServerRequest = new Promise((resolve, reject) => {
   }
 });
 
+// --
+// Handle a Fulfilled Promise with then
+// >
+// Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data").then(result => {
+  console.log(result)
+    });
+  } else {  
+    reject("Data not received");
+  }
+});
+
+
