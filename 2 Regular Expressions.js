2d2c8e8768d8f75f52e0f025fc4762973c5e6ded
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 07:04:51 +0800

    Math a Literal with diff possibilities *

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -6,3 +6,26 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 
+
+// --
+// Match a Literal String with Different Possibilities
+// >
+// Complete the regex petRegex to match the pets dog, cat, bird, or fish.
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
