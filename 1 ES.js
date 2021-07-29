dd15b85c97bc8408b069e8bb75bca28f749ae0b2
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 13:57:56 +0800

    Rest Parameter to Reassign Array Elements

--- 1 ES.js
+++ 1 ES.js
@@ -185,3 +185,15 @@ let a = 8, b = 6;
 // Only change code below this line
 [a,b] = [b,a]
 
+
+//--
+//Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+//>
+//Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted.
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const [a,b, ...arr] = list;
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
