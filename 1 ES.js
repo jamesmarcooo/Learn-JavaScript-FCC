75e3405ccd05c0257315f0ea94cc3e13f42d871c
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 14:50:21 +0800

    Literal Declarations Using Object Property Shorthand

--- 1 ES.js
+++ 1 ES.js
@@ -251,3 +251,12 @@ const failuresList = makeList(result.failure);
 // Write Concise Object Literal Declarations Using Object Property Shorthand
 // >
 // Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+const createPerson = (name, age, gender) => {
+  /*return {
+    name: name,
+    age: age,
+    gender: gender
+  };*/
+
+  return{name,age,gender};
+};
