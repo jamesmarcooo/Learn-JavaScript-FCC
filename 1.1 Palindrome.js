245583edd2dd5a7c1de9e4b299e52dd8f6288950
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 16:32:00 +0800

    Palindrome if has and with no space

--- 1.1 Palindrome.js
+++ 1.1 Palindrome.js
@@ -1,8 +1,20 @@
 function palindrome(str) {
+    let holderOrig = [];
+    let holderPal = [];
     str = str.replace(/\s/g, '');
-    console.log(str);
-    return true;
+    
+    for(let i = 0; i < str.length; i++){
+      holderOrig.push(str[i]);
+      holderPal.unshift(str[i]);
+    }
+  
+    for(let i = 0; i < str.length; i++){
+      if(holderOrig[i] != holderPal[i]){
+        return false;
+      }
+    }
+      return true;
   }
   
   
-  palindrome("eye");
+  console.log(palindrome("eye"));
