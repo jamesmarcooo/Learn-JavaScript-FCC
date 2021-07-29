ace6ceba27b30a660bd7ba709433867c79208fc0
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 15:43:54 +0800

    romise with resolve and reject

--- 1 ES.js
+++ 1 ES.js
@@ -390,3 +390,19 @@ subtract(7,4);
 // Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
 const makeServerRequest = new Promise((resolve,reject) => {});
 
+
+// --
+// Complete a Promise with resolve and reject
+// >
+// Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
