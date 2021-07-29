1d429e9bd629211be4e7eb3558fafc1c5aa7bcac
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 13:51:01 +0800

    Assign Variables from Arrays

--- 1 ES.js
+++ 1 ES.js
@@ -177,5 +177,11 @@ const LOCAL_FORECAST = {
 
 const {today:{low:lowToday, high:highToday}} = LOCAL_FORECAST;
 
-
+//--
+//Use Destructuring Assignment to Assign Variables from Arrays
+//> 
+// Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+let a = 8, b = 6;
+// Only change code below this line
+[a,b] = [b,a]
 
