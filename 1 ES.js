956f9b4ea448a22449b8765c4a92c27cd6ddcc34
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 13:43:52 +0800

    Assign Variables from Objects

--- 1 ES.js
+++ 1 ES.js
@@ -143,3 +143,20 @@ const HIGH_TEMPERATURES = {
 // const tomorrow = HIGH_TEMPERATURES.tomorrow;
 
 const {today, tomorrow} = HIGH_TEMPERATURES;
+
+//--
+//Use Destructuring Assignment to Assign Variables from Objects
+//>
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+  
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+const {today:highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+
