e3defa1fb801f12ec5528276f48247b0f23dbfb0
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 07:19:11 +0800

    Match Letters of the Alphabet

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -59,3 +59,12 @@ let result = unRegex.test(exampleStr);
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
+
+
+// --
+// Match Letters of the Alphabet
+// >
+// Match all the letters in the string quoteSample.
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
