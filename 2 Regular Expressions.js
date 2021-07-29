d5bceaf533ba3cb2de20771c727678da4a3147dd
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 07:22:16 +0800

    Match Letters of the Alphabet

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -68,3 +68,17 @@ let result = quoteSample.match(vowelRegex); // Change this line
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/ig; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
+
+// --
+// Match Numbers and Letters of the Alphabet
+// >
+// Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+
+// --
+// Match Single Characters Not Specified
+// >
+// Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
