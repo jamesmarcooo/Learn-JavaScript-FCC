6641b90fe2ee9f513bda6e4e43b31860d3810bf1
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-29 09:20:15 +0800

    Spread Operator

--- 1 ES.js
+++ 1 ES.js
@@ -117,3 +117,13 @@ const sum = (...args)=>{
     return args.reduce((a,b)=> a + b, 0);
   }
   
+// --
+// Use the Spread Operator to Evaluate Arrays In-Place
+// >
+// Copy all contents of arr1 into another array arr2 using the spread operator.
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
