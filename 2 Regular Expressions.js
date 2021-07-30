f12f129b014bc3f84ac1f17e6c7d2e2febcc9014
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 14:20:07 +0800

    / Match Everything But Letters and Numbers

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -146,3 +146,12 @@ let result = lastRegex.test(caboose);
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/gi; // Change this line
 let result = quoteSample.match(alphabetRegexV2).length;
+
+
+// --
+// Match Everything But Letters and Numbers
+// >
+// Use the shorthand character class \W to count the number of non-alphanumeric characters in various quotes and strings.
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
