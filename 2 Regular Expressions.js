7ec0ede276f1bdf440cea7f100c5f8fab37ede42
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 15:11:17 +0800

    / Check for All or None

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -233,3 +233,12 @@ let result = haRegex.test(haStr);
 let timStr = "Timmmmber";
 let timRegex = /Tim{4}ber/; // Change this line
 let result = timRegex.test(timStr);
+
+
+// --
+// Check for All or None ?
+// >
+// Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
