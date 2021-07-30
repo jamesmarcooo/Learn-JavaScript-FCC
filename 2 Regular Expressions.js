83c4c6d7c54d16bff2e140e4ecd2a0a5a5f889c9
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-30 14:26:46 +0800

    / Match All Numbers

--- 2 Regular Expressions.js
+++ 2 Regular Expressions.js
@@ -155,3 +155,11 @@ let result = quoteSample.match(alphabetRegexV2).length;
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g; // Change this line
 let result = quoteSample.match(nonAlphabetRegex).length;
+
+// --
+// Match All Numbers
+// >
+// Use the shorthand character class \d to count how many digits are in movie titles. Written out numbers ("six" instead of 6) do not count.
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
