b1976fe864dbb92e455c0ecc75e63d9942b52250
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-31 09:59:12 +0800

    / Combine Arrays with the Spread Operator

--- 3 Basic Data Structures.js
+++ 3 Basic Data Structures.js
@@ -91,3 +91,19 @@ function copyMachine(arr, num) {
   }
   
   console.log(copyMachine([true, false, true], 2));
+
+
+// --
+// Combine Arrays with the Spread Operator
+// >
+// We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning',...fragment,'is','fun'];
+    return sentence;
+  }
+  
+  console.log(spreadOut());
+
+
+--
