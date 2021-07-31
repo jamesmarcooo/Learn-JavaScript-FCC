be6c6a600f54d71032c03454d128f66af9e4aac1
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-31 09:06:41 +0800

    // Access an Array's Contents Using Bracket Notation

--- 3 Basic Data Structures.js
+++ 3 Basic Data Structures.js
@@ -4,3 +4,13 @@
 // We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
 let yourArray = ["dog",1,true,"cat",1,false]; // Change this line
 
+
+// --
+// Access an Array's Contents Using Bracket Notation
+// >
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "boy";
+// Only change code above this line
+console.log(myArray);
