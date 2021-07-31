1dba63339be1c0db0c5f15f36e395a8c94d9bdf6
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-31 09:36:53 +0800

    / Add Items Using splice()

--- 3 Basic Data Structures.js
+++ 3 Basic Data Structures.js
@@ -48,3 +48,19 @@ console.log(popShift(['challenge', 'is', 'not', 'complete']));
 // Remove Items Using splice()
 // >
 // We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+arr.splice(2,1);
+arr.splice(3,2);
+console.log(arr);
+
+
+// --
+// Add Items Using splice()
+// >
+// We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
+function htmlColorNames(arr) {
+    arr.splice(0,2,'DarkSalmon','BlanchedAlmond');
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
