2b85a4f0e5108210df7de79e1ddff65228870ea4
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-07-31 09:41:28 +0800

    / Copy Array Items Using slice() include start, exclude end

--- 3 Basic Data Structures.js
+++ 3 Basic Data Structures.js
@@ -64,3 +64,14 @@ function htmlColorNames(arr) {
   }
   
   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+// --
+// Copy Array Items Using slice()
+// >
+// We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+function forecast(arr) {
+    let r = arr.slice(2,4);
+    return r;
+}
+  
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
