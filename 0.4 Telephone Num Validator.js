dbbe858c9d3ce2ba6ea1668b74667aa7cd863ddb
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-01 15:09:06 +0800

    submitted

--- 0.4 Telephone Num Validator.js
+++ 0.4 Telephone Num Validator.js
@@ -1,9 +1,26 @@
 function telephoneCheck(str){
+    if(str.indexOf("(") === -1 && str.indexOf(")") > -1){
+        return false;
+    }
+
+    if(str[0] === "-") return false;
+    
+    if(str.indexOf(")") - str.indexOf("(") > 5){
+        return false;
+    }
+
     let polishedPhone = str.replace(/-| /g, "");
 
-    if (polishedPhone.indexOf("("< polishedPhone.indexOf(")")){
+    if (polishedPhone.indexOf("(")< polishedPhone.indexOf(")")){
         polishedPhone = polishedPhone.replace(/\(|\)/g, "");
     }
 
-    
+    if(polishedPhone.length === 10){
+        return true;
+    }else if (polishedPhone.length === 11 && polishedPhone[0] === "1"){
+        return true;
+    }
+
+    return false;
 }
+
