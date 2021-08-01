815e6f5cfe780f5b6a80403daa4abaf329c62484
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-01 11:17:56 +0800

    Roman Numeral Converter

--- 3 Basic Data Structures.js
+++ 3 Basic Data Structures.js
@@ -145,3 +145,23 @@ let myNestedArray = [
   ['mutate', 1327.98, 'splice', 'slice', 'push'],
   ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']]]
 ];
+
+
+// --
+// Add Key-Value Pairs to JavaScript Objects
+// >
+// A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods["bananas"] = 13;
+foods["grapes"]=35;
+foods["strawberries"] = 27;
+
+console.log(foods);
+
+
+--
