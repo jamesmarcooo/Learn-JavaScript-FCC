0a8891498ddd05b440c2c41802f51b53c5105076
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-02 11:37:44 +0800

    reduce with accumulator

--- 4 Advance JS.js
+++ 4 Advance JS.js
@@ -6,11 +6,15 @@ const newArray = array.forEach(num =>{
     double.push(num*2);
 });
 
-//map
+//map -- returns the array
 const mapArray = array.map(num => num*2);
 
-//filter
+//filter -- condition
 const filterArray = array.filter(num => {
     return num > 5;
 });
 
+//reduce -- remembers the last value
+const reduceArray = array.reduce(accumulator, num => {
+    return accumulator + num;
+}, 0); //accumulator starts in 0
