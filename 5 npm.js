2b916f51b1874f5311e672d423e6631199bb162f
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-03 21:11:36 +0800

    _without lodash

--- 5 npm.js
+++ 5 npm.js
@@ -1,3 +1,5 @@
 var_ = require('lodash');
 
-console.log(_); //actually, doesn't do anything
+// console.log(_); //actually, doesn't do anything
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer', _without(array, 3));
