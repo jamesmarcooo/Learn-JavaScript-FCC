527986d8f8cd366eb1bfd7313a1ee66a0dba25e7
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-03 21:12:52 +0800

    _without() method

--- 5 npm.js
+++ 5 npm.js
@@ -2,4 +2,4 @@ var_ = require('lodash');
 
 // console.log(_); //actually, doesn't do anything
 var array = [1,2,3,4,5,6,7,8];
-console.log('answer', _without(array, 3));
+console.log('answer', _without(array, 3)); //removes 3
